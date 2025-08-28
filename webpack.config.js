a3dd9b75ec3d105dff01beb3bbf15db4d3fe0d66
Alberto <albi.web@gmail.com>
2025-08-28 18:00:13 +0200

    feat: Setup webpack and initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true, // Cleans the dist folder before each build
+  },
+  devServer: {
+    static: './dist', // Serves from the dist folder
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Uses your html file as a template
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // For bundling CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
